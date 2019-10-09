429af55d511c016f10456e5d2db6258d5243e439
Yomi Toba <ytwguru@gmail.com>
2019-10-09 14:23:26 -0500

    Display context

--- functions/sender.js
+++ functions/sender.js
@@ -7,25 +7,27 @@ const sqs = new SQS({
 });
 
 export const queue = async (event, context) => {
-  const { pathParameters: { id, page } = {} } = event;
-  const accountId = context.invokedFunctionArn.split(':')[4];
-  const queueUrl = `https://sqs.us-east-1.amazonaws.com/${accountId}/${QUEUE_NAME}`;
-
-  // SQS message parameters
-  const params = {
-    MessageBody: JSON.stringify({ id, page }),
-    QueueUrl: queueUrl,
-  };
-
   try {
+    const { pathParameters: { id, page } = {} } = event;
+    const accountId = context['account-id'];
+    const queueUrl = `https://sqs.us-east-1.amazonaws.com/${accountId}/${QUEUE_NAME}`;
+
+    // SQS message parameters
+    const params = {
+      MessageBody: JSON.stringify({ id, page }),
+      QueueUrl: queueUrl,
+    };
+
     const data = await sqs.sendMessage(params).promise();
     return success({
       message: `Sent to ${queueUrl}`,
       messageId: data.MessageId,
+      context,
     });
   } catch (err) {
     return error({
       message: '',
+      context,
     });
   }
 };
