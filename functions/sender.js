162952472d9ea93432318c5410b78fb2687e2b22
Yomi Toba <ytwguru@gmail.com>
2019-10-09 15:05:13 -0500

    Fix accountId

--- functions/sender.js
+++ functions/sender.js
@@ -1,35 +1,35 @@
-// import { SQS } from 'aws-sdk';
+import { SQS } from 'aws-sdk';
 import { success, error } from '../lib/helpers/response';
 
-/* const { REGION, QUEUE_NAME } = process.env;
-const sqs = new SQS(); */
+const { REGION, QUEUE_NAME } = process.env;
+const sqs = new SQS();
 
 export const queue = async (event, context) => {
   try {
-    return success({
+    const { pathParameters: { id, page } = {} } = event;
+    const { invokedFunctionArn } = context || {};
+    const accountId = invokedFunctionArn && invokedFunctionArn.split(':')[4];
+    if (accountId) {
+      const queueUrl = `https://sqs.${REGION}.amazonaws.com/${accountId}/${QUEUE_NAME}`;
+      // SQS message parameters
+      const params = {
+        MessageBody: JSON.stringify({ id, page }),
+        QueueUrl: queueUrl,
+      };
+
+      const data = await sqs.sendMessage(params).promise();
+      return success({
+        message: `Sent to ${queueUrl}`,
+        messageId: data.MessageId,
+        context,
+      });
+    }
+    return error({
       message: '',
-      context,
     });
-    /* const { pathParameters: { id, page } = {} } = event;
-    const accountId = context['account-id'];
-    const queueUrl = `https://sqs.${REGION}.amazonaws.com/${accountId}/${QUEUE_NAME}`;
-
-    // SQS message parameters
-    const params = {
-      MessageBody: JSON.stringify({ id, page }),
-      QueueUrl: queueUrl,
-    };
-
-    const data = await sqs.sendMessage(params).promise();
-    return success({
-      message: `Sent to ${queueUrl}`,
-      messageId: data.MessageId,
-      context,
-    }); */
   } catch (err) {
     return error({
       message: '',
-      context,
     });
   }
 };
